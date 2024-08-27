3c072b389c6499d1fddb53f1ee8fb4919c7fde2e
Kiana McCullough <kianamccu@gmail.com>
2024-08-27 10:42:16 -0700

    contributors sql for handling null cases, routing name changed from people to contributors

--- apps/portal/src/config/resources.ts
+++ apps/portal/src/config/resources.ts
@@ -1,4 +1,4 @@
 export const datasetsSql = "SELECT * FROM syn62057395";
 export const filesSql = "SELECT * FROM syn61835515";
 export const publicationsSql = "SELECT * FROM syn61841834";
-export const contributorsSql = `SELECT CONCAT(firstName, ' ', lastName) AS "fullName", role, affiliation, image, summary FROM syn62530132`;
+export const contributorsSql = `SELECT TRIM(CONCAT(COALESCE(firstName, ''), ' ', COALESCE(lastName, ''))) AS "fullName", role, affiliation, image, summary FROM syn62530132`;
--- apps/portal/src/config/routesConfig.ts
+++ apps/portal/src/config/routesConfig.ts
@@ -64,7 +64,7 @@ const routes: GenericRoute[] = [
         hideRouteFromNavbar: false,
       },
       {
-        path: "People",
+        path: "Contributors",
         hideRouteFromNavbar: false,
       },
     ],
