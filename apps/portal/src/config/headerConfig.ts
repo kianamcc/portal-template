0a539007bdae7d17f687a66f0a90390c2b284eff
Kiana McCullough <kianamccu@gmail.com>
2024-08-27 17:01:11 -0700

    header description

--- apps/portal/src/config/headerConfig.ts
+++ apps/portal/src/config/headerConfig.ts
@@ -1,8 +1,9 @@
 import { HomePageHeaderConfig } from "portals-base/types/portal-config";
 
 const homePageHeader: HomePageHeaderConfig = {
-  summary: "Portal description.",
-  title: "Portal Template",
+  title: "Welcome to the Synapse Portal Template",
+  summary:
+    "This template will help you get a Synapse-integrated portal up and running in little time. Get started now and make it uniquely yours!",
 };
 
 export default homePageHeader;
